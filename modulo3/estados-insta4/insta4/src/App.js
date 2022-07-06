8b93e2f40585374076d92abbae93ed40bf87792b
SantiagoOliveira22 <santiagoc.oliveira@gmail.com>
2022-07-05 22:07:26 -0300

    Renderização de Listas 1

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -4,29 +4,30 @@ import './style.css'
 
 
 function App() {
+
+  const listaPost = [
+
+    { nomeUsuario: 'paulinha', fotoUsuario: 'https://picsum.photos/50/50', fotoPost: 'https://picsum.photos/200/150' },
+    { nomeUsuario: 'pati', fotoUsuario: 'https://picsum.photos/200/200', fotoPost: 'https://picsum.photos/200/152' },
+    { nomeUsuario: 'fabi', fotoUsuario: 'https://picsum.photos/20/20', fotoPost: 'https://picsum.photos/200/154' }
+  ]
+
+  let listaComponentes = listaPost.map((post, index) => {
+    return (
+      <Post key={post.index}
+        nomeUsuario={post.nomeUsuario}
+        fotoUsuario={post.fotoUsuario}
+        fotoPost={post.fotoPost}
+      />
+    )
+  })
+
   return (
     <div className='MainContainer'>
-      <Post
-        nomeUsuario={'paulinha'}
-        fotoUsuario={'https://picsum.photos/50/50'}
-        fotoPost={'https://picsum.photos/200/150'}
-      />
-
-      <Post
-        nomeUsuario={'pati'}
-        fotoUsuario={'https://picsum.photos/200/200'}
-        fotoPost={'https://picsum.photos/200/152'}
-      />
-
-      <Post
-        nomeUsuario={'fabi'}
-        fotoUsuario={'https://picsum.photos/20/20'}
-        fotoPost={'https://picsum.photos/200/154'}
-      />
+      {listaComponentes}
+      {console.log(listaPost)}
     </div>
   )
-
 }
-
 
 export default App;
