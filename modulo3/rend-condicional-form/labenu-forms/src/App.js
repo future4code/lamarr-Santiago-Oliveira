6f3470d5dc1f1fd962c96caebc94d59d63d841d0
SantiagoOliveira22 <santiagoc.oliveira@gmail.com>
2022-07-12 22:32:42 -0300

    Renderização Condicional

---
+++ modulo3/rend-condicional-form/labenu-forms/src/App.js
@@ -0,0 +1,18 @@
+import { Etapa1 } from "./components/Etapa1/Etapa1";
+import { Etapa2 } from "./components/Etapa2/Etapa2";
+import { Etapa3 } from "./components/Etapa3/Etapa3";
+import { Final } from "./components/Final/Final";
+
+
+function App() {
+  return (
+    <div>
+      <Etapa1/>
+      <Etapa2/>
+      <Etapa3/>
+      <Final/>
+    </div>
+  );
+}
+
+export default App;
