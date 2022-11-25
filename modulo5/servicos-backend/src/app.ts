9104204d153b67c3682a5c8d096e8fbc8cbe55de
SantiagoOliveira22 <santiagoc.oliveira@gmail.com>
2022-11-24 21:15:40 -0300

    Inicado exercicio Serviços no Backend

---
+++ modulo5/servicos-backend/src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen(process.env.PORT || 3003, () => {
+  console.log(`Server is running in ${process.env.PORT || 3003}`);
+});
+
+export default app;
