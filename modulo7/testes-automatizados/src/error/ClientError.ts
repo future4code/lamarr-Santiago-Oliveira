8db0136a654eb2e3cd95f2d67d73403276f691ed
SantiagoOliveira22 <santiagoc.oliveira@gmail.com>
2023-03-03 13:15:50 -0300

    Finalizado exercício

--- modulo7/testes-automatizados/src/error/ClientError.ts
+++
@@ -1,7 +0,0 @@
-import { CustomError } from "./CustomError";
-
-export class InvalidName extends CustomError{
-    constructor(){
-        super(422,"Nome Inválido!")
-    }
-} 
--- modulo7/testes-automatizados/src/error/CustomError.ts
+++
@@ -1,5 +0,0 @@
-export class CustomError extends Error{
-    constructor(public statusCod:number,public message:string){
-        super(message)
-    }
-}
